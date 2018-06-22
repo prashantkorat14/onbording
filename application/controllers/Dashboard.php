c8ab67e0c00c56f50d07aacf3a3f852e5b51b1ea
Prashant Korat <prashantkorat14@gmail.com>
2018-06-22 23:50:17 +0530

    doe update

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,14 +1,19 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Dashboard extends CI_Controller
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
+
+        $this->load->helper('url');
     }
 
-    function login() {
+    public function index()
+    {
         $pageData = array();
         $pageData['template'] = 'panel/dashboard/page';
 
--- application/views/layout.php
+++ application/views/layout.php
@@ -24,11 +24,11 @@
 
     </head>
     <body class="home">
-        <?php $this->load->view('elements/header') ?>
+        <?php $this->load->view('elements/header')?>
 
-        <?php $this->load->view($template) ?>
+        <?php $this->load->view($template)?>
 
-        <?php $this->load->view('elements/footer') ?>
+        <?php $this->load->view('elements/footer')?>
 
         <!-- Include all compiled plugins (below), or include individual files as needed -->
         <script src="<?php echo base_url('assets/front/'); ?>js/bootstrap.min.js"></script>
@@ -44,6 +44,40 @@
             });
         </script>
 
+<script>
+  (function(apiKey){
+    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
+      v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){
+      o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+      y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
+      z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+
+    // Call this whenever information about your visitors becomes available
+    // Please use Strings, Numbers, or Bools for value types.
+    pendo.initialize({
+      visitor: {
+        id:              'VISITOR-UNIQUE-ID'   // Required if user is logged in
+        // email:        // Optional
+        // role:         // Optional
+
+        // You can add any additional visitor level key-values here,
+        // as long as it's not one of the above reserved names.
+      },
+
+      account: {
+        id:           'ACCOUNT-UNIQUE-ID' // Highly recommended
+        // name:         // Optional
+        // planLevel:    // Optional
+        // planPrice:    // Optional
+        // creationDate: // Optional
+
+        // You can add any additional account level key-values here,
+        // as long as it's not one of the above reserved names.
+      }
+    });
+  })('');
+</script>
 
     </body>
+
 </html>
